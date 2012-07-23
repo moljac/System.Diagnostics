61f24e42803255d23282380990d4517b6863b73e
moljac <mcvjetko@holisticware.net>
2012-07-23 10:55:39 +0200

    Initial commit

---
+++ System.Diagnostics.MA/DiagnosticsConfiguration.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Configuration;
+using System.Threading;
+
+namespace System.Diagnostics
+{
+
+	internal class DiagnosticsConfiguration
+	{
+		public static object Settings { get; set; }
+	}
+}
